c4bbd1b715d6c1b2c4b186b065f69425f4e832a4
fillype <fillype1@hotmail.com>
2023-05-22 09:45:13 -0300

    refactor code for services controller

--- app/controllers/clinic_management/services_controller.rb
+++ app/controllers/clinic_management/services_controller.rb
@@ -4,95 +4,12 @@ module ClinicManagement
     include TimeSlotsHelper
     # GET /services
     def index
-      @services = ClinicManagement::Service.all
-      @rows = @services.order(:date).reverse.each_with_index.map do |ser,index|
-        total_appointments = ser.appointments.count
-        scheduled = ser.appointments.where(attendance: true).count
-        rescheduled = ser.appointments.where(status: "remarcado").count
-        canceleds = ser.appointments.where(status: "cancelado").count
-        [
-          { header: "#", content: index + 1 },
-          { header: "Data", content: ser.date.strftime("%d/%m/%Y") },
-          { header: "Dia da semana", content: helpers.show_week_day(ser.weekday) },
-          { header: "Início", content: ser.start_time.strftime("%H:%M") },
-          { header: "Fim", content: ser.end_time.strftime("%H:%M") },
-          { header: "Pacientes", content: total_appointments },
-          { 
-            header: "Presentes", 
-            content: "#{scheduled} <span class='bg-blue-200 text-blue-700 p-1 rounded'>(#{(scheduled.to_f/total_appointments*100).round(2)}%)</span>".html_safe, 
-            class: "text-blue-700" 
-          },
-          { 
-            header: "Remarcados", 
-            content: "#{rescheduled} <span class='bg-green-200 text-green-700 p-1 rounded'>(#{(rescheduled.to_f/total_appointments*100).round(2)}%)</span>".html_safe, 
-            class: "text-green-600" 
-          },
-          { 
-            header: "Cancelados", 
-            content: "#{canceleds} <span class='bg-red-200 text-red-700 p-1 rounded'>(#{(canceleds.to_f/total_appointments*100).round(2)}%)</span>".html_safe, 
-            class: "text-red-600" 
-          },                
-          { header: "Detalhes", content: helpers.link_to("Detalhes", ser, class: "text-blue-500 hover:text-blue-700") }
-        ]        
-      end
+      @rows = process_services_data(ClinicManagement::Service.includes(:appointments).order(date: :desc))
     end
 
     # GET /services/1
     def show
-      @appointments = @service.appointments.includes(:invitation).sort_by { |ap| ap.invitation.patient_name }
-      @rows = @appointments.map.with_index(1) do |ap, index|
-        [
-          { header: "#", content: index },
-          { header: "Paciente", content: ap.invitation.patient_name },
-          { header: "Telefone", content: ap.lead.phone },
-          { header: "Endereço", content: ap.invitation.lead.address },
-          { header: "Região", content: ap.invitation.region.name },
-          { header: "Indicação", content: ap.invitation.referral.name },
-          { 
-            header: "Status", 
-            content: ap.status,
-            id: "status-#{ap.id}", 
-            class: case ap.status
-                    when "agendado"
-                      "text-yellow-600"
-                    when "remarcado"
-                      "text-orange-600"
-                    when "cancelado"
-                      "text-red-600"
-                    else
-                      ""
-                    end
-          },          
-          { 
-            header: "Comparecimento", 
-            content: ap.attendance ? "Sim" : "Não", 
-            id: "attendance-#{ap.id}", 
-            class: ap.attendance ? "text-green-600" : "text-red-600" 
-          },          
-          { header: "Nº de Comparecimentos", content: ap.lead.appointments.count },
-          { 
-            header: "Ação", 
-            content: set_appointment_button(ap), 
-            id: "set-attendance-button-#{ap.id}", 
-            class: "pt-2 pb-0"
-          },          
-          { header: "Observações", content: ap.invitation.notes },
-          { 
-            header: "Mensagem", 
-            content: render_to_string(partial: "clinic_management/lead_messages/lead_message_form", locals: { lead: ap.lead }),
-            id: "whatsapp-link-" + ap.lead.id.to_s
-          },
-          { header: "Remarcação", content: "", class: "text-orange-500" },
-          { 
-            header: "Cancelar?", 
-            content: set_cancel_button(ap), 
-            id: "cancel-attendance-button-#{ap.id}", 
-            class: "pt-2 pb-0"
-            },
-          { header: "Cliente?", content: "", class: "text-purple-500" }
-        ]                 
-      end
-      
+      @rows = process_appointments_data(@service.appointments.includes(:invitation, :lead))       
     end
 
     # GET /services/new
@@ -135,12 +52,101 @@ module ClinicManagement
 
     private
 
+    def process_appointments_data(appointments)
+      sorted_appointments = appointments.sort_by { |ap| ap.invitation.patient_name }
+      
+      sorted_appointments.map.with_index(1) do |ap, index|
+        attendance_class, status_class = appointment_classes(ap)
+  
+        [
+          { header: "#", content: index },
+          { header: "Paciente", content: ap.invitation.patient_name },
+          { header: "Telefone", content: ap.lead.phone },
+          { header: "Endereço", content: ap.invitation.lead.address },
+          { header: "Região", content: ap.invitation.region.name },
+          { header: "Indicação", content: ap.invitation.referral.name },
+          { header: "Status", content: ap.status, id: "status-#{ap.id}", class: status_class },          
+          { header: "Comparecimento", content: ap.attendance ? "Sim" : "Não", id: "attendance-#{ap.id}", class: attendance_class },          
+          { header: "Nº de Comparecimentos", content: ap.lead.appointments.count },
+          { header: "Ação", content: set_appointment_button(ap), id: "set-attendance-button-#{ap.id}", class: "pt-2 pb-0" },          
+          { header: "Observações", content: ap.invitation.notes },
+          { header: "Mensagem", content: generate_message_content(ap), id: "whatsapp-link-#{ap.lead.id}" },
+          { header: "Remarcação", content: "", class: "text-orange-500" },
+          { header: "Cancelar?", content: set_cancel_button(ap), id: "cancel-attendance-button-#{ap.id}", class: "pt-2 pb-0" },
+          { header: "Cliente?", content: "", class: "text-purple-500" }
+        ]                 
+      end
+    end
+  
+    def appointment_classes(appointment)
+      attendance_class = appointment.attendance ? "text-green-600" : "text-red-600"
+  
+      status_class = case appointment.status
+                     when "agendado"
+                       "text-yellow-600"
+                     when "remarcado"
+                       "text-orange-600"
+                     when "cancelado"
+                       "text-red-600"
+                     else
+                       ""
+                     end
+  
+      [attendance_class, status_class]
+    end
+  
+    def generate_message_content(appointment)
+      render_to_string(
+        partial: "clinic_management/lead_messages/lead_message_form",
+        locals: { lead: appointment.lead }
+      )
+    end
+
+    def process_services_data(services)
+      services.map.with_index do |ser, index|
+        total_appointments, scheduled, rescheduled, canceleds = appointment_counts(ser)
+        [
+          { header: "#", content: index + 1 },
+          { header: "Data", content: ser.date.strftime("%d/%m/%Y") },
+          { header: "Dia da semana", content: helpers.show_week_day(ser.weekday) },
+          { header: "Início", content: ser.start_time.strftime("%H:%M") },
+          { header: "Fim", content: ser.end_time.strftime("%H:%M") },
+          { header: "Pacientes", content: total_appointments },
+          percentage_content("Presentes", scheduled, total_appointments, "text-blue-700", "bg-blue-200"),
+          percentage_content("Remarcados", rescheduled, total_appointments, "text-green-600", "bg-green-200"),
+          percentage_content("Cancelados", canceleds, total_appointments, "text-red-600", "bg-red-200"),
+          { header: "Detalhes", content: helpers.link_to("Detalhes", ser, class: "text-blue-500 hover:text-blue-700") }
+        ]
+      end
+    end
+
     def set_appointment_button(ap)
       if ap.attendance.present?
         "--"
       else
         helpers.button_to('Marcar como presente', set_attendance_appointment_path(ap), method: :patch, remote: true, class: "py-2 px-4 bg-blue-500 text-white rounded hover:bg-blue-700")
       end
+    end
+
+    def appointment_counts(service)
+      appointments = service.appointments
+      total_appointments = appointments.count
+      scheduled = appointments.where(attendance: true).count
+      rescheduled = appointments.where(status: "remarcado").count
+      canceleds = appointments.where(status: "cancelado").count
+      [total_appointments, scheduled, rescheduled, canceleds]
+    end
+  
+    def percentage_content(header, count, total, text_class, bg_class)
+      {
+        header: header,
+        content: "#{count} <span class='#{bg_class} #{text_class} p-1 rounded'>(#{percentage(count, total)}%)</span>".html_safe,
+        class: text_class
+      }
+    end
+  
+    def percentage(count, total)
+      (count.to_f / total * 100).round(2)
     end
 
     def set_cancel_button(ap)
