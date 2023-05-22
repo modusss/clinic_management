a03ba473fbc9d8d02edf9cd8c8b9750005587e0f
fillype <fillype1@hotmail.com>
2023-05-22 09:44:51 -0300

    refactor code fir invitation controller

--- app/controllers/clinic_management/invitations_controller.rb
+++ app/controllers/clinic_management/invitations_controller.rb
@@ -4,7 +4,7 @@ module ClinicManagement
 
     # GET /invitations
     def index
-      @invitations = Invitation.all
+      @rows = process_invitations_data(Invitation.all.includes(:lead, :region, appointment: :service).order(date: :desc))
     end
 
     # GET /invitations/1
@@ -57,6 +57,32 @@ module ClinicManagement
     end
 
     private
+
+      def process_invitations_data(invitations)
+        invitations.map do |invite|
+          last_appointment = invite.lead.appointments.last
+          [
+            {header: "Data do convite", content: invite.date.strftime("%d/%m/%Y")},
+            {header: "Convitre para", content: helpers.link_to(invite_day(invite), service_path(invite.appointment.service), class: "text-blue-500 hover:text-blue-700", target: "_blank" )},
+            {header: "Nome do paciente", content: invite.patient_name},
+            {header: "Nome do responsável", content: responsible_content(invite)},   
+            {header: "Telefone", content: invite.lead.phone},
+            {header: "Observação", content: invite.notes},
+            {header: "Indicação", content: invite.referral.name},
+            {header: "Vezes que foi convidado", content: invite.lead.appointments.count},
+            {header: "Região", content: invite.region.name}
+          ]
+        end
+      end
+
+      def invite_day(invite)
+        service = invite.appointment.service
+        helpers.show_week_day(service.time_slot.weekday) + " " + service.date.strftime("%d/%m") + ", " + service.start_time.strftime("%H:%M") + "h às " + service.end_time.strftime("%H:%M") + "h"
+      end
+
+      def responsible_content(invite)
+        (invite.lead.name != invite.patient_name) ? invite.lead.name : ""
+      end
 
       def associate_appointment_with_lead
         appointment = @invitation.appointment
