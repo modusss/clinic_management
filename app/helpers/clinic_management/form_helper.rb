4a319cd53a8d5ce6c09c81b6e98d34123d6cc1a2
fillype <fillype1@hotmail.com>
2023-05-26 09:11:58 -0300

    fix excluded segment of form_helper

--- app/helpers/clinic_management/form_helper.rb
+++ app/helpers/clinic_management/form_helper.rb
@@ -56,3 +56,44 @@ module ClinicManagement
         end
     end
 end
+
+
+class StandardizedFormBuilder < ActionView::Helpers::FormBuilder
+    FORM_FIELD_CLASS = 'appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4'.freeze
+
+    def label(method, text = nil, options = {}, &block)
+        return if method.nil?
+        options[:class] = "#{options[:class]} block text-gray-700 font-bold mb-2"
+        super
+    end
+    
+    def text_field(method, options = {})
+        return if method.nil?
+        options[:class] = "#{options[:class]} #{FORM_FIELD_CLASS}"
+        super
+    end
+
+    def text_area(method, options = {})
+        return if method.nil?
+        options[:class] = "#{options[:class]} #{FORM_FIELD_CLASS}"
+        super
+    end
+    
+    def select(method, choices = nil, options = {}, html_options = {}, &block)
+        return if method.nil?
+        html_options[:class] = "#{html_options[:class]} appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mb-4"
+        super
+    end
+
+    def date_field(method, options = {})
+        return if method.nil?
+        options[:class] = "#{options[:class]} #{FORM_FIELD_CLASS}"
+        super
+    end   
+
+    def submit(value=nil, options={})
+        options[:class] = "#{options[:class]} bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        super
+    end
+      
+end
