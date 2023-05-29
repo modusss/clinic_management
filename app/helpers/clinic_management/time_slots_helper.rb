c80804f61abb484632e0865a5cefe5798bfebbd3
fillype <fillype1@hotmail.com>
2023-05-29 14:28:00 -0300

    fix next_30_days_time_slots function

--- app/helpers/clinic_management/time_slots_helper.rb
+++ app/helpers/clinic_management/time_slots_helper.rb
@@ -1,30 +1,20 @@
 module ClinicManagement
   module TimeSlotsHelper
 
+    
     def next_30_days_time_slots
-      week_days = {
-        1 => 'Domingo',
-        2 => 'Segunda-feira',
-        3 => 'Terça-feira',
-        4 => 'Quarta-feira',
-        5 => 'Quinta-feira',
-        6 => 'Sexta-feira',
-        7 => 'Sábado'
-      }
+      time_slots = ClinicManagement::TimeSlot.all.group_by(&:weekday)
+      existing_services = Service.where(date: Date.today..Date.today + 29.days)
+                                 .pluck(:weekday, :date, :start_time)
+                                 .map { |ws| [ws[0..1], ws[2]] }
+                                 .to_h
     
-      time_slots = ClinicManagement::TimeSlot.all
-      next_30_days = (Date.today..Date.today + 29.days)
-    
-      options = next_30_days.flat_map do |date|
-        weekday = date.wday == 6 ? 7 : date.wday + 1  # ajuste aqui para fazer a correspondência correta
-        weekday_slots = time_slots.select { |slot| slot.weekday == weekday }
+      options = (Date.today..Date.today + 29.days).flat_map do |date|
+        weekday = date.wday == 6 ? 7 : date.wday + 1
+        weekday_slots = time_slots[weekday] || []
         weekday_slots.map do |slot|
-          next if Service.exists?(
-            weekday: slot.weekday,
-            date: date,
-            start_time: slot.start_time
-          )
-          display_info = "#{date.strftime('%d/%m/%Y')} - #{week_days[slot.weekday]} - #{slot.start_time.strftime('%H:%M')} até #{slot.end_time.strftime('%H:%M')}"
+          next if existing_services[[weekday, date]] == slot.start_time
+          display_info = "#{date.strftime('%d/%m/%Y')} - #{I18n.t('date.day_names')[date.wday]} - #{slot.start_time.strftime('%H:%M')} até #{slot.end_time.strftime('%H:%M')}"
           value = {time_slot_id: slot.id, date: date}.to_json
           [display_info, value]
         end
@@ -32,6 +22,7 @@ module ClinicManagement
     
       {type: 'select', selected: "", input: :time_slot_id, name: 'Dias e horários disponíveis', options: options}
     end
+    
     
 
   end
