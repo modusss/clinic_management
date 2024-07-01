7e149d65e10a875ffba86366219ecd2d90a2637b
Fillype Farias <fillype1@hotmail.com>
2024-07-01 10:45:00 +0200

    add more options for lead_message?

--- app/models/clinic_management/lead_message.rb
+++ app/models/clinic_management/lead_message.rb
@@ -1,6 +1,14 @@
 module ClinicManagement
     class LeadMessage < ApplicationRecord
-      enum message_type: { confirmação: 0, remarcação: 1, lembrete: 2, outro: 3 }
+      enum message_type: { confirmação: 0, 
+                           remarcação: 1, 
+                           lembrete: 2, 
+                           recuperação_mesmo_dia: 4,
+                           recuperação_dois_dias: 5,
+                           recuperação_sete_dias: 6,
+                           recuperação_quinze_dias: 7,
+                           recuperação_dois_meses: 8,
+                           outro: 3 }
       belongs_to :service_type, optional: true
     end
 end
