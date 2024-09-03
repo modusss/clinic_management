846ed6b55c1a0bde0b21be595a8fc4c81a959302
Fillype Farias <fillype1@hotmail.com>
2024-09-03 17:25:27 -0300

    add grouped message

--- app/helpers/clinic_management/messages_helper.rb
+++ app/helpers/clinic_management/messages_helper.rb
@@ -2,7 +2,7 @@ module ClinicManagement
   module MessagesHelper
 
     def get_lead_messages(lead, appointment)
-      LeadMessage.all
+      LeadMessage.includes(:service_type).group_by { |m| m.service_type&.name || 'Sem categoria' }
     end
 
   end
