9448b3b19f749b6bc7bfcbf00c9e0ebf5a19e76e
modusss <fillype1@hotmail.com>
2025-02-28 11:36:48 -0300

    Modify leads table to render appointment comments dynamically
    - Updated the leads table to render appointment comments using a partial view
    - Reordered table columns to improve the display of appointment information
    - Replaced static comments display with a dynamic rendering approach

--- app/controllers/clinic_management/leads_controller.rb
+++ app/controllers/clinic_management/leads_controller.rb
@@ -253,10 +253,10 @@ module ClinicManagement
         row = [
           {header: "#", content: index + 1},
           {header: "Paciente", content: invitation&.patient_name, class: "nowrap size_20" },
-          {header: "Data do atendimento", content: service_content},         
+          {header: "Data do atendimento", content: service_content},
+          {header: "Observações", content: render_to_string(partial: "clinic_management/shared/appointment_comments", locals: { appointment: ap, message: "" }), id: "appointment-comments-#{ap.id}"},                   
           {header: "Remarcação", content: reschedule_form(new_appointment, ap), class: "text-orange-500"},
           {header: "Comparecimento", content: (ap.attendance == true ? "Sim" : "Não"), class: helpers.attendance_class(ap)},
-          {header: "Observações", content: ap.comments },
           {header: "Status", content: ap.status, class: helpers.status_class(ap)},
           {header: "Data do convite", content: invitation&.created_at&.strftime("%d/%m/%Y")},
           {header: "Região", content: invitation&.region&.name}
