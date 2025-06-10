751d6e80b9b912082f8dae336055be06540918c8
modusss <fillype1@hotmail.com>
2025-06-10 14:46:12 -0300

    Update invitations_controller to include phone check in skip_before_action
    - Modified the `skip_before_action` in `invitations_controller.rb` to include the `check_existing_phone` action, ensuring that the referral user redirection is bypassed during phone validation processes.
    - This change enhances the functionality of the invitation management system by allowing for seamless phone checks without unnecessary redirection, improving user experience and operational efficiency.

--- app/controllers/clinic_management/invitations_controller.rb
+++ app/controllers/clinic_management/invitations_controller.rb
@@ -1,7 +1,7 @@
 module ClinicManagement
   class InvitationsController < ApplicationController
     before_action :set_invitation, only: %i[ show edit update destroy ]
-    skip_before_action :redirect_referral_users, only: [:new, :create, :update, :index, :edit_patient_name, :update_patient_name]
+    skip_before_action :redirect_referral_users, only: [:new, :create, :update, :index, :edit_patient_name, :update_patient_name, :check_existing_phone]
     include GeneralHelper
 
     # GET /invitations
