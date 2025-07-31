7dd67227e2d31ebc68b14a6aaeb3e0ad483fb23d
modusss <fillype1@hotmail.com>
2025-07-31 17:24:35 -0300

    feat: add conditional logic for Evolution API access based on user type and instance status

--- app/helpers/clinic_management/messages_helper.rb
+++ app/helpers/clinic_management/messages_helper.rb
@@ -14,5 +14,20 @@ module ClinicManagement
       end
     end
 
+    # Check if Evolution API can be used based on user type and instance connection status
+    def can_use_evolution_api?(message_id = nil)
+      return false unless message_id.present?
+      
+      if referral?(current_user)
+        # For referral users, check if their instance is connected
+        referral = user_referral
+        referral&.instance_connected == true
+      else
+        # For non-referral users, check if instance 2 is connected
+        membership = current_user.memberships.last
+        membership&.instance_2_connected == true
+      end
+    end
+
   end
 end
