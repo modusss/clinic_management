dad47f32be172a20f1a43334439e7f24d0df7cf6
modusss <fillype1@hotmail.com>
2025-08-01 15:31:19 -0300

    refactor: simplify Evolution API availability check by removing message_id dependency

--- app/helpers/clinic_management/messages_helper.rb
+++ app/helpers/clinic_management/messages_helper.rb
@@ -15,17 +15,15 @@ module ClinicManagement
     end
 
     # Check if Evolution API can be used based on user type and instance connection status
-    def can_use_evolution_api?(message_id = nil)
-      return false unless message_id.present?
-      
+    def can_use_evolution_api?
+      #return false unless message_id.present?
       if referral?(current_user)
         # For referral users, check if their instance is connected
         referral = user_referral
         referral&.instance_connected == true
       else
         # For non-referral users, check if instance 2 is connected
-        membership = current_user.memberships.last
-        membership&.instance_2_connected == true
+        Account.first.instance_2_connected == true
       end
     end
 
